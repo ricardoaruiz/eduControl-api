6358c7ece244c1073f42ecccb326c07e7531b333
Ricardo Almendro Ruiz <ricardo.almendro.ruiz@gmail.com>
2016-08-04 21:47:33 -0300

    Ajustando base de dados para o heroku

--- app/infra/ConnectionFactory.js
+++ app/infra/ConnectionFactory.js
@@ -2,12 +2,24 @@ var mysql = require('mysql');
 
 var object = {
     getConnection : function() {
-        return mysql.createConnection({
-            user : 'root',
-            password : 'root',
-            host : 'localhost',
-            database : 'eduControl'
-        });
+        if(!process.env.NODE_ENV | process.env.NODE_ENV === 'development') {
+            return mysql.createConnection({
+                user : 'root',
+                password : 'root',
+                host : 'localhost',
+                database : 'eduControl'
+            });
+        }
+
+        if(process.env.NODE_ENV === 'production') {
+            return mysql.createConnection({
+                user : 'b02e0832da6d1d',
+                password: '65912b5a',
+                host : 'us-cdbr-iron-east-04.cleardb.net',
+                database : 'heroku_bf2733d210a0d62'
+            });
+        } 
+
     }
 };
 
