8cbd4c79869eaaa00c641bceb8f7049521222675
Ricardo Almendro Ruiz <ricardo.almendro.ruiz@gmail.com>
2016-08-04 22:15:47 -0300

    Ajustando a porta

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('./config/express');
 
 var app = express();
 
-app.set('port', 9100);
+var port = process.env.PORTA || 9100
 
-app.listen(app.get('port'), function() {
-    console.log('Edu-control backend rodando em ' + app.get('port'));
+app.listen(port, function() {
+    console.log('Edu-control backend rodando em ' + port);
 });
